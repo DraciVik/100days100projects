b7c75cf013955d02c59caa284d10946376d3b26b
DraciVik <viktor.stojanov90@gmail.com>
2020-01-29 22:59:02 +0100

    Added mouseover and mouseout event listeners and applied appropriate logic to add and remove background-color

--- hover-board/src/App.js
+++ hover-board/src/App.js
@@ -1,26 +1,54 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  componentDidMount() {
+    const myDiv = document.querySelectorAll(".div");
+    myDiv.forEach(div => {
+      div.addEventListener("mouseover", this.addRandomBackground);
+      div.addEventListener("mouseout", this.removeRandomBackground);
+    });
+  }
+
+  removeRandomBackground = e => {
+    let currentDiv = e.currentTarget;
+    // e.currentTarget.transition: "2s ease"
+    // currentDiv.style.transition = "2s ease";
+    currentDiv.style.background = "#dec8fa";
+    // this.removeTransition(e);
+  };
+  removeTransition = e => {
+    let currentDiv = e.currentTarget;
+    // e.currentTarget.transition: "2s ease"
+    currentDiv.style.transition = "none";
+  };
+
+  addRandomBackground = e => {
+    let randomRGBValue1 = Math.floor(Math.random() * 256);
+    let randomRGBValue2 = Math.floor(Math.random() * 256);
+    let randomRGBValue3 = Math.floor(Math.random() * 256);
+
+    let backgroundValue = `rgb(${randomRGBValue1}, ${randomRGBValue2}, ${randomRGBValue3})`;
+    let currentDiv = e.currentTarget;
+    currentDiv.style.background = backgroundValue;
+  };
+
+  consoleThis = () => {
+    return console.log("hi");
+  };
+
+  render() {
+    let arrayOfDivs = new Array(480).fill(5);
+    return (
+      <div className="App">
+        <main id="container">
+          {arrayOfDivs.map((div, index) => (
+            <div className="div" key={index}></div>
+          ))}
+        </main>
+      </div>
+    );
+  }
 }
 
 export default App;
