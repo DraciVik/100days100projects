1ab6f8c358ee36806d27662b5a8e2c3cc20c1563
DraciVik <viktor.stojanov90@gmail.com>
2020-01-31 00:42:02 +0100

    Added Day1 components and styling for tablet and pc

--- project-showcase/src/App.js
+++ project-showcase/src/App.js
@@ -1,24 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Day1 from "./components/Day1";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="shocaseHeader">
+        <h1>#100DaysOfCode - project showcase</h1>
       </header>
+      <main id="container">
+        <Day1 />
+        <Day1 />
+        <Day1 />
+        <Day1 />
+
+        <Day1 />
+
+        <Day1 />
+
+        <Day1 />
+
+        <Day1 />
+        <Day1 />
+      </main>
     </div>
   );
 }
---
+++ project-showcase/src/components/Day1.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Day1() {
+  return (
+    <article class="card">
+      <img src="https://source.unsplash.com/random/300x400" alt="random" />
+      <section>
+        <h4>React Calculator - Day: 001</h4>
+      </section>
+    </article>
+  );
+}
